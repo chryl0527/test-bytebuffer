0560eb37a0cbdb0fe905c6c2aaee5b347af02df8
chr0527 <1015670600@qq.com>
2019-05-23 16:58:16 +0800

    init

--- src/main/java/com/sdsoon/byteBuf/Demo.java
+++ src/main/java/com/sdsoon/byteBuf/Demo.java
@@ -15,25 +15,28 @@ public class Demo {
     public static void main(String args[]) {
 
         //netty-byteBuf
-        ByteBuf byteBuf = Unpooled.buffer(42);
+        ByteBuf byteBuf = Unpooled.buffer(88);
 
         String s = "FE0A4A4E3132715067734D34374550534976099CFE";
         byteBuf.writeBytes(s.getBytes());
 
-        System.out.println(byteBuf);
+        byte[] bb = new byte[byteBuf.writerIndex()];
+        byteBuf.readBytes(bb);//类似于 byteBuffer.get(b);
+        String str = new String(bb);
+        System.out.println(str);
+        System.out.println("byteBuf:" + byteBuf);
+        byteBuf.discardReadBytes();//忽略已读
+        System.out.println("byteBuf:" + byteBuf);
 
 
+        //#######################################################//上面已经discard已读的数据了
+        byteBuf.writeBytes(s.getBytes());//再次转化
         //byteBuf->byteBuffer
         ByteBuffer byteBuffer = byteBuf.nioBuffer();
-        System.out.println(byteBuffer);
-
-
-
-
+        System.out.println("byteBuffer:" + byteBuffer);
         //写到byte[]
         byte[] b = new byte[byteBuffer.remaining()];
         byteBuffer.get(b);
-
         String string = new String(b);
         System.out.println(string);
 
